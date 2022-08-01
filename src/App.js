7be61e372d036f9af2589dca3b8ae942f2fe8cb8
Ming You <a9650615@gmail.com>
2022-08-02 02:45:20 +0800

    feat: add react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-  
+    <div className='App'>
+      <Routes>
+        <Route path='/' />
+      </Routes>
     </div>
   );
 }
